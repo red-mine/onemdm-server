288a4122fde533f855a26aed5b955bda8a661a2d
hunt <hunt@kali>
2025-03-04 14:18:26 +0800

    Figure out app_usage_params issues

--- app/controllers/app_usages_controller.rb
+++ app/controllers/app_usages_controller.rb
@@ -1,24 +1,23 @@
 class AppUsagesController < ApplicationController
-  before_filter :authenticate_device
+  before_action :authenticate_device
   respond_to :json
-  
+
   def create
     begin
       @device.app_usages << AppUsage.create!(app_usage_params)
       render json: {}, status: :created
-    rescue Exception => e  
+    rescue Exception => e
       logger.warn "Error while saving App Usage #{e.message}"
-      render json: {}, 
+      render json: {},
               status: :unprocessable_entity
     end
   end
   private
 
   def app_usage_params
-    params.permit(app_usage: 
-                  [:package_name,
-                    :usage_duration_in_seconds,
-                    :used_on]).require(:app_usage)
+    params.require(:app_usage).permit(app_usage: [:package_name,
+                                                  :usage_duration_in_seconds,
+                                                  :used_on])
 
   end
 end
