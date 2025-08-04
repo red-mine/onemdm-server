959aff5ed8e70b0365b9b569ad5246a5814b8398
Hunt Lin <hunt.lin@tes-tec.com>
2025-08-04 14:27:57 +0800

    Recent Pushes for OTA

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -15,7 +15,7 @@ ActiveAdmin.register_page "Dashboard" do
       end
     end
 
-    panel "Recent Pushes" do
+    panel "Recent Pushes for App" do
       table_for AppBatchInstallation.order('id desc').limit(10) do
         column "Pushed on" do |batch|
           batch.created_at
@@ -50,5 +50,42 @@ ActiveAdmin.register_page "Dashboard" do
         end
       end
     end
+
+    panel "Recent Pushes for OTA" do
+      table_for PkgBatchInstallation.order('id desc').limit(10) do
+        column "Pushed on" do |batch|
+          batch.created_at
+        end
+        column "OTA Name" do |batch|
+          batch.pkg.name
+        end
+        column "# Devices" do |batch|
+          link_to batch.pkg_installations.count,
+            admin_devices_path(q: {pkg_installations_pkg_batch_installation_id_eq: batch.id})
+        end
+        column "# Installed" do |batch|
+          link_to batch.pkg_installations.installed.count,
+            admin_devices_path(q: {pkg_installations_pkg_batch_installation_id_eq: batch.id,
+                                 installations_status_eq: PkgInstallation.statuses[:installed]})
+        end
+        column "# Cancelled" do |batch|
+          link_to batch.pkg_installations.cancelled.count,
+            admin_devices_path(q: {pkg_installations_pkg_batch_installation_id_eq: batch.id,
+                                 installations_status_eq: PkgInstallation.statuses[:cancelled]})
+        end
+        column "# Pending" do |batch|
+          link_to batch.pkg_installations.pushed.count + batch.pkg_installations.downloaded.count,
+             admin_devices_path(q: {pkg_installations_pkg_batch_installation_id_eq: batch.id,
+                                 installations_status_in: [PkgInstallation.statuses[:pushed],
+                                                           PkgInstallation.statuses[:downloaded]]})
+        end
+        column "% Success" do |batch|
+          total = batch.pkg_installations.count
+          installed = batch.pkg_installations.installed.count
+          percentage_success = (installed/total) * 100 if total > 0
+        end
+      end
+    end
+
   end # content
 end
