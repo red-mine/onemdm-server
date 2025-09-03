4f81f6b3a337ab9acac43179478b8e7314bb4164
Hunt Lin <hunt.lin@tes-tec.com>
2025-09-03 14:58:40 +0800

    hide

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -57,22 +57,22 @@ ActiveAdmin.register_page "Dashboard" do
       end
     end
 
-    panel "Recent Pushes for App" do
-      table_for AppBatchInstallation.order('id desc').limit(10) do
-        column("Pushed on", &:created_at)
-        column("App Name") { |batch| batch.app.name }
-        column("# Devices")   { |b| link_to b.app_installations.count, admin_devices_path(q: { app_installations_app_batch_installation_id_eq: b.id }) }
-        column("# Installed") { |b| link_to b.app_installations.installed.count, admin_devices_path(q: { app_installations_app_batch_installation_id_eq: b.id, installations_status_eq: AppInstallation.statuses[:installed] }) }
-        column("# Cancelled") { |b| link_to b.app_installations.cancelled.count, admin_devices_path(q: { app_installations_app_batch_installation_id_eq: b.id, installations_status_eq: AppInstallation.statuses[:cancelled] }) }
-        column("# Pending")   { |b| link_to(b.app_installations.pushed.count + b.app_installations.downloaded.count, admin_devices_path(q: { app_installations_app_batch_installation_id_eq: b.id, installations_status_in: [AppInstallation.statuses[:pushed], AppInstallation.statuses[:downloaded]] })) }
-        column "% Success" do |b|
-          total = b.app_installations.count
-          installed = b.app_installations.installed.count
-          pct = total.zero? ? 0.0 : (installed.to_f / total * 100.0)
-          number_to_percentage(pct, precision: 1)
-        end
-      end
-    end
+    # panel "Recent Pushes for App" do
+    #   table_for AppBatchInstallation.order('id desc').limit(10) do
+    #     column("Pushed on", &:created_at)
+    #     column("App Name") { |batch| batch.app.name }
+    #     column("# Devices")   { |b| link_to b.app_installations.count, admin_devices_path(q: { app_installations_app_batch_installation_id_eq: b.id }) }
+    #     column("# Installed") { |b| link_to b.app_installations.installed.count, admin_devices_path(q: { app_installations_app_batch_installation_id_eq: b.id, installations_status_eq: AppInstallation.statuses[:installed] }) }
+    #     column("# Cancelled") { |b| link_to b.app_installations.cancelled.count, admin_devices_path(q: { app_installations_app_batch_installation_id_eq: b.id, installations_status_eq: AppInstallation.statuses[:cancelled] }) }
+    #     column("# Pending")   { |b| link_to(b.app_installations.pushed.count + b.app_installations.downloaded.count, admin_devices_path(q: { app_installations_app_batch_installation_id_eq: b.id, installations_status_in: [AppInstallation.statuses[:pushed], AppInstallation.statuses[:downloaded]] })) }
+    #     column "% Success" do |b|
+    #       total = b.app_installations.count
+    #       installed = b.app_installations.installed.count
+    #       pct = total.zero? ? 0.0 : (installed.to_f / total * 100.0)
+    #       number_to_percentage(pct, precision: 1)
+    #     end
+    #   end
+    # end
 
     panel "Recent Pushes for OTA" do
       table_for PkgBatchInstallation.order('id desc').limit(10) do
