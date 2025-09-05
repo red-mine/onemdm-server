61d460aed4f44f2c3d9de2c8745343f6c3fe32ed
Hunt Lin <hunt.lin@tes-tec.com>
2025-09-05 11:47:50 +0800

    commit

--- config/initializers/00_product_name.rb
+++ config/initializers/00_product_name.rb
@@ -1,4 +1,3 @@
 # Centralized product naming
 # Use ENV['PRODUCT_NAME'] to override without code changes.
-Rails.application.config.x.product_name = ENV.fetch('PRODUCT_NAME', 'TES OverAir')
-
+Rails.application.config.x.product_name = ENV.fetch('PRODUCT_NAME', 'tes-tec TOTA')
